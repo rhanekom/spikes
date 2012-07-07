9dedaa2d0a686ff762c519097fd35a79b10dbd6e
Riaan Hanekom <rhanekom@gmail.com>
2012-07-07 21:37:44 +0200

    nuget tests

---
+++ nuget-package-tests/ngenerics/NGenericsPackageTest/NGenericsPackageTest.Console/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NGenerics.DataStructures.General;
+
+namespace NGenericsPackageTest.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var graph = new Graph<int>(false);
+        }
+    }
+}
